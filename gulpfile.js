7a33bacd86ee0a0de5e260481ee972ca3c217774
Stewart Park <hello@stewartjpark.com>
2016-07-09 22:59:52 -0700

    Download bower dependencies and build JS

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,17 @@ var sassPaths = [
   'bower_components/motion-ui/src'
 ];
 
+var jsPaths = [
+  'bower_components/jquery/dist/jquery.js',
+  'bower_components/what-input/what-input.js',
+  'bower_components/foundation-sites/dist/foundation.js',
+  'js/app.js'
+];
+
+gulp.task('bower', function() {
+  return $.run('bower install').exec();
+});
+
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
@@ -16,9 +27,17 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('css'));
+    .pipe($.rename({basename: 'brand.css'}))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('js', function() {
+  return gulp.src(jsPaths)
+    .pipe($.concat({path: 'brand.js', stat: {mode: 0666}}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['sass', 'js'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['js/**/*.js'], ['js']);
 });
